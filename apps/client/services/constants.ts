cc93f606d93f510fe9d2767b2bfbe959df346448
baraa <hey.baraa@gmail.com>
2022-11-30 15:58:29 +0200

    fix .env

--- apps/client/services/constants.ts
+++ apps/client/services/constants.ts
@@ -4,4 +4,9 @@ export interface DefaultMutationProps {
   onSettled?: () => void
 }
 
-export const SECURITY_SERVICE_URL = process.env.NODE_PUBLIC_SECURITY_SERVICE_URL ?? 'http://localhost:3050'
+export const SHOP_SERVICE_URL = process.env.NEXT_PUBLIC_SHOP_SERVICE_URL ?? 'http://localhost:3000'
+export const SHOP_CONSUMER_SERVICE_URL = process.env.NEXT_PUBLIC_SHOP_CONSUMER_SERVICE_URL ?? 'http://localhost:3010'
+export const RESERVATIONS_SERVICE_URL = process.env.NEXT_PUBLIC_RESERVATIONS_SERVICE_URL ?? 'http://localhost:3020'
+export const ANALYTICS_SERVICE_URL = process.env.NEXT_PUBLIC_ANALYTICS_SERVICE_URL ?? 'http://localhost:3030'
+export const PAYMENTS_SERVICE_URL = process.env.NEXT_PUBLIC_PAYMENTS_SERVICE_URL ?? 'http://localhost:3040'
+export const SECURITY_SERVICE_URL = process.env.NEXT_PUBLIC_SECURITY_SERVICE_URL ?? 'http://localhost:3050'
