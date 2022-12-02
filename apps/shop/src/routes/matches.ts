f60d673f27225a5e4233f14d717719365cc00496
Youssef Saad <youssef.elbasha.saad@gmail.com>
2022-12-03 00:14:52 +0200

    added route for get-upcoming-games method

--- apps/shop/src/routes/matches.ts
+++ apps/shop/src/routes/matches.ts
@@ -2,11 +2,14 @@ import express from 'express'
 import getMatches from '../controllers/matches/get-matches'
 import getMatch from '../controllers/matches/get-match'
 import getMatchSummary from '../controllers/matches/get-match-summary'
+import getUpcomingMatches from '../controllers/matches/get-upcoming-matches'
 
 const router = express.Router()
 
 router.get('/', getMatches)
+router.get('/upcoming', getUpcomingMatches)
 router.get('/:id', getMatch)
 router.get('/:id/summary', getMatchSummary)
+
 
 export default router
