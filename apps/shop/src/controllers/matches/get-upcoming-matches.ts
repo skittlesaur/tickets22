43754fbcb4969a629fc3ceb8906751ce61b2813f
baraa <hey.baraa@gmail.com>
2022-12-03 08:04:16 +0200

    formatting

--- apps/shop/src/controllers/matches/get-upcoming-matches.ts
+++ apps/shop/src/controllers/matches/get-upcoming-matches.ts
@@ -1,26 +1,23 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
 const getUpcomingMatches = async (req: Request, res: Response) => {
-    try {
+  try {
+    const upcomingMatches = await req.context.prisma.match.findMany({
+      where: {
+        date: {
+          gte: new Date(),
+        },
+      },
+      orderBy: {
+        date: 'asc',
+      },
+      take: 4,
+    })
 
-        const upcomingMatches = await req.context.prisma.match.findMany({
-            where: {
-                date: {
-                    gte: new Date()
-                }
-            },
-            orderBy: {
-                date: 'asc'
-            },
-            take: 4
-        })
+    res.status(200).json({ upcomingMatches })
+  } catch (error) {
 
-        res.status(200).json({ upcomingMatches })
-
-
-    } catch (error) {
-
-    }
+  }
 }
 
 export default getUpcomingMatches
