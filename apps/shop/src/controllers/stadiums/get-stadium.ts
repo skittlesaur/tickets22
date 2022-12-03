353a8455f85aec6817276c372956188a9daa1e18
baraa <hey.baraa@gmail.com>
2022-12-03 08:06:08 +0200

    formatting

--- apps/shop/src/controllers/stadiums/get-stadium.ts
+++ apps/shop/src/controllers/stadiums/get-stadium.ts
@@ -1,43 +1,39 @@
-import { Request, Response } from "express";
-import { CLIENT_URL } from '../../constants'
+import { Request, Response } from 'express'
 
 const getStadium = async (req: Request, res: Response) => {
-    try {
+  try {
+    const { id } = req.params
 
-        const { id } = req.params
+    if (!id)
+      return res.status(400).json({
+        message: 'Missing stadium id',
+        details: 'The stadium id is missing from the request parameters',
+        code: 'invalid_parameters',
+        help: '',/*todo later*/
+      })
 
-        if (!id)
-            return res.status(400).json({
-                message: 'Missing stadium id',
-                details: 'The stadium id is missing from the request parameters',
-                code: 'invalid_parameters',
-                help: '',/*todo later*/
-            })
+    const stadium = await req.context.prisma.stadium.findUnique({
+      where: { id: id },
+      select: {
+        id: true,
+        name: true,
+        capacity: true,
+      },
+    })
 
-
-        const stadium = await req.context.prisma.stadium.findUnique({
-            where: { id: id },
-            select: {
-                id: true,
-                name: true,
-                capacity: true
-            }
-        })
-
-        if (!stadium) {
-            return res.status(404).json({
-                message: 'Stadium not found',
-                details: 'The stadium with the given id does not exist',
-                code: 'not_found',
-                help: '',/*todo later*/
-            })
-        }
-
-        return res.status(200).json(stadium)
-
-    } catch (e) {
-
+    if (!stadium) {
+      return res.status(404).json({
+        message: 'Stadium not found',
+        details: 'The stadium with the given id does not exist',
+        code: 'not_found',
+        help: '',/*todo later*/
+      })
     }
+
+    return res.status(200).json(stadium)
+  } catch (e) {
+
+  }
 
 }
 
--- apps/shop/src/routes/stadiums.ts
+++ apps/shop/src/routes/stadiums.ts
@@ -1,9 +1,8 @@
 import express from 'express'
-import getStadium from "../controllers/stadiums/get-stadium"
+import getStadium from '../controllers/stadiums/get-stadium'
 
 const router = express.Router()
 
-router.get('/', (req, res) => { return res.status(200).json({ 'message': 'root' }) })
 router.get('/:id', getStadium)
 
 export default router
