572f21102af07b741f72734cbd31f13107899a0d
baraa <hey.baraa@gmail.com>
2022-12-29 00:26:09 +0200

    teams page

--- apps/client/components/teams/main/team-card.tsx
+++ apps/client/components/teams/main/team-card.tsx
@@ -5,17 +5,20 @@ import { useState } from 'react'
 
 const TeamCard = ({ team }: any) => {
   const [hover, setHover] = useState(false)
+
   return (
-    <div className="relative">
-      <SquareIcon className="absolute inset-0 text-secondary group-hover:text-primary" />
+    <div className="relative w-52 aspect-square">
+      <SquareIcon className={`absolute inset-0 stroke-primary dark:stroke-secondary/50 ${hover ? 'text-primary dark:text-gray-700' : 'text-secondary dark:text-gray-800'} transition-all duration-200 ease-in-out`} />
       <Link
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
         href={`/teams/${team.id}`}
-        className="relative z-[1] flex flex-col items-center gap-4 p-10"
+        className="relative z-[1] flex flex-col items-center gap-4 p-10 hover:text-secondary transition-all duration-200 ease-in-out"
       >
-        <div className="w-20 aspect-square">
+        <div className={`w-20 aspect-square ${hover ? 'drop-shadow-xl' : ''} transition-all duration-200 ease-in-out`}>
           {getTeamIcon(team.name)}
         </div>
-        <h2 className="font-qatar text-2xl">{team.name}</h2>
+        <h2 className="font-qatar text-2xl text-center">{team.name}</h2>
       </Link>
     </div>
   )
