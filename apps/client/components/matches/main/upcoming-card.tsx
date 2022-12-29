c35be6b9cb6457b41705316b07e272e55f2a34b4
baraa <hey.baraa@gmail.com>
2022-12-29 16:22:40 +0200

    build issue

--- apps/client/components/matches/main/upcoming-card.tsx
+++ apps/client/components/matches/main/upcoming-card.tsx
@@ -8,7 +8,6 @@ interface UpcomingCardProps {
 
 const UpcomingCard = ({ match }: UpcomingCardProps) => {
   const { data: homePlayer, isLoading: homePlayerLoading } = useRandomPlayerQuery(match.homeTeam.matchNumber)
-  console.log(homePlayer)
   const { data: awayPlayer, isLoading: awayPlayerLoading } = useRandomPlayerQuery(match.awayTeam.matchNumber)
 
   if (!match)
