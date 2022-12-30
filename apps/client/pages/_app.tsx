61a7af11ad9db31accf28ef8dfaf5c5d397ccecb
baraa <hey.baraa@gmail.com>
2022-12-30 16:52:37 +0200

    progress bar

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -5,6 +5,18 @@ import toast, { Toaster } from 'react-hot-toast'
 import { ThemeProvider } from 'next-themes'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
+
+const progress = new ProgressBar({
+  size: 3,
+  className: 'bar-of-progress',
+  delay: 100,
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 const queryClient = new QueryClient()
 
